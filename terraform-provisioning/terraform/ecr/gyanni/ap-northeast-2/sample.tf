0ba1c3a7f113cd21f30c977269c02ca70df2eb59
Gyanni <lghlee36@gmail.com>
2026-02-03 00:26:24 +0900

    add ecr repo

--- terraform-provisioning/terraform/ecr/gyanni/ap-northeast-2/sample.tf
+++ terraform-provisioning/terraform/ecr/gyanni/ap-northeast-2/sample.tf
@@ -1,12 +1,12 @@
-resource "aws_ecr_repository" "sample" {
-  name = "sample"
+resource "aws_ecr_repository" "tfplayground" {
+  name = "tfplayground"
   image_scanning_configuration {
     scan_on_push = true
   }
   image_tag_mutability = "IMMUTABLE"
 }
-resource "aws_ecr_repository_policy" "sample" {
-  repository = aws_ecr_repository.sample.name
+resource "aws_ecr_repository_policy" "tfplayground" {
+  repository = aws_ecr_repository.tfplayground.name
   policy = jsonencode({
     "Version" : "2008-10-17",
     "Statement" : [
