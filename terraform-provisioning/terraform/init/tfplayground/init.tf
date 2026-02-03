92b439f7dafcc8de554fbcf96c8ae13720dcd339
Gyanni <lghlee36@gmail.com>
2026-02-03 13:31:45 +0900

    add s3 backend for init module

--- terraform-provisioning/terraform/init/tfplayground/init.tf
+++ terraform-provisioning/terraform/init/tfplayground/init.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "tfplayground-apnortheast2-tfstate"
+    key            = "init/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
 variable "assume_role_arn" {
   description = "The role to assume when accessing the AWS API."
   default     = ""
