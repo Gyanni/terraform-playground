efcf4a6cdc28bf1e07dc1395e51e76fed38d7a0f
Gyanni <lghlee36@gmail.com>
2026-02-04 18:16:07 +0900

    fix sg backend

--- terraform-provisioning/terraform/securitygroup/gyanni/tpgd_apnortheast2/backend.tf
+++ terraform-provisioning/terraform/securitygroup/gyanni/tpgd_apnortheast2/backend.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     bucket         = "tfplayground-apnortheast2-tfstate"
-    key            = "provisioning/terraform/securitygroup/gyanni/tmcd_apnortheast2/terraform.tfstate"
+    key            = "provisioning/terraform/securitygroup/gyanni/tpgd_apnortheast2/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
     dynamodb_table = "terraform-lock-table"
