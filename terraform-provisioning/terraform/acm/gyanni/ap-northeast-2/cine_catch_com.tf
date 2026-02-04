92f50da7d4f7aaeecf4cf336564c5c4abd29733a
Gyanni <lghlee36@gmail.com>
2026-02-04 19:40:00 +0900

    add acm

--- terraform-provisioning/terraform/acm/gyanni/ap-northeast-2/cine_catch_com.tf
+++ terraform-provisioning/terraform/acm/gyanni/ap-northeast-2/cine_catch_com.tf
@@ -1,9 +1,9 @@
 module "star_terraform_devart_tv" {
   source = "../_modules/acm"
 
-  domain_name = "*.terraform.devart.tv"
+  domain_name = "cine-catch.com"
   zone_id     = var.r53_variables.terraform_devart_tv_zone_id
-  subject_alternative_names          = ["terraform.devart.tv"]
+  subject_alternative_names          = ["cine-catch.com"]
   validation_allow_overwrite_records = true
   wait_for_validation                = true
 }
