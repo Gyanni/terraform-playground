32c23d123e7ec0a4473070c78f010cd0dadab1e2
Gyanni <lghlee36@gmail.com>
2026-02-04 19:42:18 +0900

    fix backend file

--- terraform-provisioning/terraform/acm/gyanni/ap-northeast-2/backend.tf
+++ terraform-provisioning/terraform/acm/gyanni/ap-northeast-2/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "s3" {
-    bucket         = "gyanni-apnortheast2-tfstate"
+    bucket         = "tfplayground-apnortheast2-tfstate"
     key            = "provisioning/terraform/acm/gyanni/ap-northeast-2/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
