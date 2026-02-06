83ba8d66fcffbfd72a7715d067340655bc17f88f
Gyanni <lghlee36@gmail.com>
2026-02-06 16:42:17 +0900

    fix zone id

--- terraform-provisioning/terraform/acm/gyanni/ap-northeast-2/cine_catch_com.tf
+++ terraform-provisioning/terraform/acm/gyanni/ap-northeast-2/cine_catch_com.tf
@@ -2,7 +2,7 @@ module "star_terraform_devart_tv" {
   source = "../_modules/acm"
 
   domain_name = "cine-catch.com"
-  zone_id     = var.r53_variables.terraform_devart_tv_zone_id
+  zone_id     = var.r53_variables.cine_catch_zone_id
   subject_alternative_names          = ["cine-catch.com"]
   validation_allow_overwrite_records = true
   wait_for_validation                = true
